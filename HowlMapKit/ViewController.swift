db0770c82afe8cf76a4b91a7aa92cbff0eff034e
MyeongSic <you6878@icloud.com>
2017-06-01 21:58:31 +0900

    init

--- HowlMapKit/ViewController.swift
+++ HowlMapKit/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapKitView: MKMapView!
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        
+        let location = CLLocationCoordinate2D(latitude: 37.392448, longitude: 126.638831)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Central Park"
+        annotation.subtitle = "공원"
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapKitView.setRegion(region, animated: true)
+        mapKitView.addAnnotation(annotation)
+        
+        
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapKitView.showsUserLocation = true
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
